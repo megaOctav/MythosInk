39c37fadda4f34d5de545d9dbedb74b3b72f4232
Mega Octaviana <megap9039@gmail.com>
2025-03-13 08:29:28 +0700

    mythosink

--- asset/script.js
+++ asset/script.js
@@ -27,15 +27,21 @@ anchor.addEventListener('click', function(e) {
 });
 
 document.querySelectorAll('.footer_tag a').forEach(anchor => {
-anchor.addEventListener('click', function(e) {
-  e.preventDefault();
-  const targetId = this.getAttribute('href').substring(1);
-  const targetSection = document.getElementById(targetId);
-
-  // Scroll to the section smoothly
-  window.scrollTo({
-      top: targetSection.offsetTop - 50,  // adjust the offset for navbar height
-      behavior: 'smooth'
+    anchor.addEventListener('click', function(e) {
+      // Cek apakah link mengarah ke luar website
+      if (this.hostname !== window.location.hostname) {
+        return; // Biarkan default behavior untuk link eksternal
+      }
+  
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+  
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: 'smooth'
+        });
+      }
+    });
   });
-});
-});
